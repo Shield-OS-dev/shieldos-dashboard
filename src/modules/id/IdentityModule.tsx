4741fd8e65f1dd62f82f95c8b7cdecf110710302
Shield OS <shiledosdev@gmail.com>
2025-11-19 11:23:33 +0700

    Create IdentityModule.tsx

---
+++ src/modules/id/IdentityModule.tsx
@@ -0,0 +1,11 @@
+export interface IdentityModuleConfig {
+  id: "id";
+  label: "Identity";
+  description: string;
+}
+
+export const IdentityModule: IdentityModuleConfig = {
+  id: "id",
+  label: "Identity",
+  description: "Identity and permission management."
+};

08b744e59f366365c38e49bb1a9e66135381d048
Shield OS <shiledosdev@gmail.com>
2025-11-19 11:22:23 +0700

    Create DriveModule.tsx

---
+++ src/modules/drive/DriveModule.tsx
@@ -0,0 +1,11 @@
+export interface DriveModuleConfig {
+  id: "drive";
+  label: "Drive";
+  description: string;
+}
+
+export const DriveModule: DriveModuleConfig = {
+  id: "drive",
+  label: "Drive",
+  description: "Encrypted storage for files and folders."
+};

c3b32829ceae24b2270c98741c2a1df874458fb5
Shield OS <shiledosdev@gmail.com>
2025-11-19 11:22:48 +0700

    Create VaultModule.tsx

---
+++ src/modules/vault/VaultModule.tsx
@@ -0,0 +1,11 @@
+export interface VaultModuleConfig {
+  id: "vault";
+  label: "Vault";
+  description: string;
+}
+
+export const VaultModule: VaultModuleConfig = {
+  id: "vault",
+  label: "Vault",
+  description: "Passwords and secrets manager."
+};

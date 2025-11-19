a139b6cd1a0eb81ff691f6cd8f49e1c76f08e046
Shield OS <shiledosdev@gmail.com>
2025-11-19 11:21:59 +0700

    Create MailModule.tsx

---
+++ src/modules/mail/MailModule.tsx
@@ -0,0 +1,11 @@
+export interface MailModuleConfig {
+  id: "mail";
+  label: "Mail";
+  description: string;
+}
+
+export const MailModule: MailModuleConfig = {
+  id: "mail",
+  label: "Mail",
+  description: "Encrypted inbox and communication layer."
+};
